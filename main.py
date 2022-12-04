02c596273986730ded5a196cde9bafcde569e387
thulanimbatha <thulanimbatha06@gmail.com>
2022-12-05 01:33:28 +0200

    version 1.0

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    blog_posts = response.json()
+    return render_template("index.html", posts=blog_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
