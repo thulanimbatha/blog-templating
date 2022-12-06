1a24d2e20e37002c59fc50a0b254775aa25fadb9
thulanimbatha <thulanimbatha06@gmail.com>
2022-12-07 00:45:06 +0200

    completed - version 2.0

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 import requests
 
+blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
+response = requests.get(url=blog_url)
+blog_posts = response.json()    # list
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
-    response = requests.get(url=blog_url)
-    blog_posts = response.json()
     return render_template("index.html", posts=blog_posts)
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    wanted_post = None
+    for post in blog_posts:
+        if post['id'] == index:
+            wanted_post = post
+    return render_template("post.html", post=wanted_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- post.py
+++ post.py
@@ -1,2 +1,20 @@
 class Post:
-    pass
+    
+    def __init__(self,id,title,subtitle,body):
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
+
+    # getters
+    def get_id(self):
+        return self.id
+    
+    def get_title(self):
+        return self.title
+    
+    def get_subtitle(self):
+        return self.subtitle
+
+    def get_body(self):
+        return self.body
